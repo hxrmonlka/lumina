a8328795b1705e6616b9a8012542c07bd75798dd
Ota <127403802+hxrmonlka@users.noreply.github.com>
2026-09-05 21:27:44 +0800

    fixed this.

--- modules/lib/parts.nix
+++ modules/lib/parts.nix
@@ -1,7 +1,14 @@
 {lib, ...}: {
-  options.flake.lib = lib.mkOption {
-    type = lib.types.lazyAttrsOf lib.types.unspecified;
-    default = {};
-    description = "Lumina's library, consumed by Alice via the lumina flake input.";
+  options.flake = {
+    lib = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.unspecified;
+      default = {};
+      description = "Lumina's library, consumed by Alice via the lumina flake input.";
+    };
+    homeModules = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.unspecified;
+      default = {};
+      description = "Home Manager modules exported by Lumina.";
+    };
   };
 }
